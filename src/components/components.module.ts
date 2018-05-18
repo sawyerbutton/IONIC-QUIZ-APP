76c538054970e099f32ad9f527630e3ddf1ebec3
sawyerbutton <lil103@pitt.edu>
2018-05-17 23:38:12 -0400

    modified the procfile

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { FlashCardComponent } from './flash-card/flash-card';
-@NgModule({
-	declarations: [FlashCardComponent],
-	imports: [],
-	exports: [FlashCardComponent]
-})
-export class ComponentsModule {}
